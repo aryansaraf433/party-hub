9f7e51d244bd293e8284a92a89af24f4f8c4bec2
Aryan Saraf <rudrasaraf22@gmail.com>
2026-06-06 13:48:43 +0530

    fix: Rebuilt Uno frontend with correct environment variables

---
+++ server/public/uno/precache-manifest.3b11cbe7a2805074cbcd8e7ef76e1f48.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aae012cdb5c5e0185321dafc3fb5b4b",
+    "url": "/uno/android-chrome-192x192.png"
+  },
+  {
+    "revision": "8e5c2cf3ad43c0479acf9452502f1ba0",
+    "url": "/uno/android-chrome-512x512.png"
+  },
+  {
+    "revision": "26571786c486d15d03774ac3e8de3c7e",
+    "url": "/uno/apple-touch-icon.png"
+  },
+  {
+    "revision": "e9f985e1f6d0d777c483ae0a60ddb54f",
+    "url": "/uno/browserconfig.xml"
+  },
+  {
+    "revision": "3ffb00476687feba80f6",
+    "url": "/uno/css/app.21b8d26e.css"
+  },
+  {
+    "revision": "6e0dd35576f534ac0eaaa27ead532bf4",
+    "url": "/uno/favicon-16x16.png"
+  },
+  {
+    "revision": "c78d09df08bac12a99fb3f6d558a5bfd",
+    "url": "/uno/favicon-32x32.png"
+  },
+  {
+    "revision": "16c49c632a51c3867a3a143c789c63e2",
+    "url": "/uno/favicon.ico"
+  },
+  {
+    "revision": "d1738bd678a0ff0e03f80b32f7e5a1b9",
+    "url": "/uno/favicon.png"
+  },
+  {
+    "revision": "af8fff7b56f462dd7653a69823ff1714",
+    "url": "/uno/img/arrow.af8fff7b.jpg"
+  },
+  {
+    "revision": "e18024b6a253bd901c2175b0e671d2e1",
+    "url": "/uno/img/back.e18024b6.png"
+  },
+  {
+    "revision": "bb20ae0f6adc0e05f17f36689aa1b4bb",
+    "url": "/uno/img/logo.bb20ae0f.png"
+  },
+  {
+    "revision": "704e2abd2366822f3f2b645147c77647",
+    "url": "/uno/img/online.704e2abd.jpg"
+  },
+  {
+    "revision": "97ad0f84509f1f52465aad4b43991302",
+    "url": "/uno/img/plus.97ad0f84.jpg"
+  },
+  {
+    "revision": "7a9e5b44684a973ef271c096b75ad9f2",
+    "url": "/uno/img/rooms.7a9e5b44.jpg"
+  },
+  {
+    "revision": "02b12df76d93339ca32d0c54891e3ef3",
+    "url": "/uno/img/settings.02b12df7.jpg"
+  },
+  {
+    "revision": "4f8ad9ff68b87657b6fb157a546e1d5d",
+    "url": "/uno/img/solo.4f8ad9ff.jpg"
+  },
+  {
+    "revision": "78003784b171da04f471e76bb07ef61f",
+    "url": "/uno/img/spritesheet.78003784.jpg"
+  },
+  {
+    "revision": "d1a2c36cadc71782d0e28c6285cdc4fc",
+    "url": "/uno/index.html"
+  },
+  {
+    "revision": "3ffb00476687feba80f6",
+    "url": "/uno/js/app.94683b64.js"
+  },
+  {
+    "revision": "f0a5f4683a67e91a6d36",
+    "url": "/uno/js/chunk-vendors.f14b719a.js"
+  },
+  {
+    "revision": "517ffcf95f046139bb2ad6ae1c8462f9",
+    "url": "/uno/manifest.json"
+  },
+  {
+    "revision": "c8f4a2f708a6ec453f0e891bc54070ba",
+    "url": "/uno/mstile-150x150.png"
+  },
+  {
+    "revision": "1e40d2513f6a96d592b26f900d7e1e45",
+    "url": "/uno/normalize.css"
+  },
+  {
+    "revision": "4bd96415f2a5b03617487945b0de9b9f",
+    "url": "/uno/remove-manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/uno/robots.txt"
+  },
+  {
+    "revision": "4d036e217d5e54cb0653c490e8c67279",
+    "url": "/uno/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "68b59645ffd3979056acebab7dd727d3",
+    "url": "/uno/service-worker.js"
+  }
+]);
--- server/public/uno/service-worker.js
+++ server/public/uno/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/uno/precache-manifest.59fb1596832dbfa31d06073c44810270.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/uno/precache-manifest.3b11cbe7a2805074cbcd8e7ef76e1f48.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
