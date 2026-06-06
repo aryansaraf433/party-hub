6e6b48f921a75390ad9e72451935f5897c3f5d47
Aryan Saraf <rudrasaraf22@gmail.com>
2026-06-06 13:40:34 +0530

    chore: Built Uno frontend with production URL

--- server/public/uno/service-worker.js
+++ server/public/uno/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/uno/precache-manifest.df62afa63958a0ad2887a6165b877a12.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/uno/precache-manifest.59fb1596832dbfa31d06073c44810270.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
